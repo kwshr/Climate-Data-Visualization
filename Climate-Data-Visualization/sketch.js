81275657f84766ce4f015171c1250860c3966add
Kawshar <kwshrptl@gmail.com>
2023-11-03 13:22:56 -0400

    Modify files

--- Climate-Data-Visualization/sketch.js
+++ Climate-Data-Visualization/sketch.js
@@ -2,18 +2,14 @@ let table;
 let selectedColumn = 'Year'; 
 let startYear = 1880;
 let endYear = 1880; 
-let selectyear;
+let selectYear;
 let maxData, minData;
 let isDrawing = false; 
 let frameRateValue =1;
 let currentYear = 1880;
-let positiveSound;
-let negativeSound;
 
 function preload() {
   table = loadTable('data/data.csv', 'csv', 'header');
-   positiveSound = loadSound('data/positive.wav'); 
-  negativeSound = loadSound('data/negative.wav'); 
 }
 
 function setup() {
@@ -28,7 +24,7 @@ function setup() {
     drop.option(col);
   }
   drop.selected(selectedColumn);
-  selectyear = drop;
+  selectYear = drop;
 }
 
 function draw() {
@@ -43,19 +39,14 @@ function draw() {
     let x = width / (maxYear - startIndex);
     let dataRange = max(maxData, -minData);
     let y = height / dataRange / 2; 
-    let ySet = height / 2;
+    let setY = height / 2;
     noFill();
     stroke(0);
     for (let i = startIndex; i < maxYear; i++) {
       const x1 = (i - startIndex) * x;
-      const y1 = ySet - selectedData[i] * y;
+      const y1 = setY - selectedData[i] * y;
       const x2 = (i - startIndex + 1) * x;
-      const y2 = ySet - selectedData[i + 1] * y;
-      if (selectedData[i] > 0) {
-    positiveSound.play();
-  } else if (selectedData[i] < 0) {
-    negativeSound.play();
-  }
+      const y2 = setY - selectedData[i + 1] * y;
       strokeWeight(2);
       stroke(random(255), random(255), random(255));
       line(x1, y1, x2, y2);
@@ -65,7 +56,6 @@ function draw() {
     }
     currentYear++;
   }
-  
   strokeWeight(1);
   textSize(15);
   text('CURRENT YEAR: ' + currentYear, 10, 50);
@@ -76,7 +66,7 @@ function getColumnData(columnName) {
 }
 
 function updateSelectedColumn() {
-  selectedColumn = selectyear.value();
+  selectedColumn = selectYear.value();
   startYear = 1880;
   endYear = 2022;
   isDrawing = true;
